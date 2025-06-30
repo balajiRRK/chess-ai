af5fbf14e9f7077ce4a6b96cca2a0393d7b2051a
balajiRRK <rbalajirrk@gmail.com>
2025-06-30 15:48:35 -0400

    Add rough piece movement

---
+++ chess_game.py
@@ -0,0 +1,134 @@
+import pygame
+
+pygame.init()
+
+class Chess:
+    def __init__(self, screen_width=800, screen_height=800, block_size=100):
+        self.SCREEN_WIDTH = screen_width
+        self.SCREEN_HEIGHT = screen_height
+        self.BLOCK_SIZE = block_size
+
+        self.window = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        pygame.display.set_icon(pygame.image.load('pieces/white_king.png'))  # Set a default icon
+        pygame.display.set_caption("Chess")
+
+        self.selected_pos = None
+        self.players_turn = 'white' 
+
+        # load piece images
+        self.pieces = {
+            'white': {
+                'king': pygame.transform.scale(pygame.image.load('pieces/white_king.png'), (100, 100)),
+                'queen': pygame.transform.scale(pygame.image.load('pieces/white_queen.png'), (100, 100)),
+                'rook': pygame.transform.scale(pygame.image.load('pieces/white_rook.png'), (100, 100)),
+                'bishop': pygame.transform.scale(pygame.image.load('pieces/white_bishop.png'), (100, 100)),
+                'knight': pygame.transform.scale(pygame.image.load('pieces/white_knight.png'), (100, 100)),
+                'pawn': pygame.transform.scale(pygame.image.load('pieces/white_pawn.png'), (100, 100))
+            },
+            'black': {
+                'king': pygame.transform.scale(pygame.image.load('pieces/black_king.png'), (100, 100)),
+                'queen': pygame.transform.scale(pygame.image.load('pieces/black_queen.png'), (100, 100)),
+                'rook': pygame.transform.scale(pygame.image.load('pieces/black_rook.png'), (100, 100)),
+                'bishop': pygame.transform.scale(pygame.image.load('pieces/black_bishop.png'), (100, 100)),
+                'knight': pygame.transform.scale(pygame.image.load('pieces/black_knight.png'), (100, 100)),
+                'pawn': pygame.transform.scale(pygame.image.load('pieces/black_pawn.png'), (100, 100))
+            }
+        }
+
+        # setup board state with pieces
+        self.board = [[None for _ in range(8)] for _ in range(8)]
+
+        # Initial positions for each piece type
+        piece_order = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
+        for x in range(8):
+            self.board[0][x] = ('black', piece_order[x])
+            self.board[1][x] = ('black', 'pawn')
+            # `None` inbetween
+            self.board[6][x] = ('white', 'pawn')
+            self.board[7][x] = ('white', piece_order[x])
+
+        # draw board state
+
+        self.draw_board()
+        self.draw_pieces()
+
+    def draw_board(self):
+        brown = (180, 135, 98)
+        white = (255, 255, 255)
+
+        for y in range(0, 8):
+            for x in range(0, 8):
+                if (x + y) % 2 == 0:
+                    color = white
+                else:
+                    color = brown
+                pygame.draw.rect(self.window, color, (x * self.BLOCK_SIZE, y * self.BLOCK_SIZE, self.BLOCK_SIZE, self.BLOCK_SIZE))
+        return
+    
+    def draw_pieces(self):
+        for y in range(8):
+            for x in range(8):
+                piece = self.board[y][x]
+                if piece is not None:
+                    color, type = piece
+                    self.window.blit(self.pieces[color][type], (x * self.BLOCK_SIZE, y * self.BLOCK_SIZE))
+        return   
+
+    def select_piece(self, y, x):
+        # if piece exists on clicked square and if players_turn matches the color of the piece selected
+        if self.board[y][x] is not None and self.players_turn == self.board[y][x][0]:
+            self.selected_pos = y, x
+            pygame.draw.rect(self.window, (255, 0, 0), (x * self.BLOCK_SIZE, y * self.BLOCK_SIZE, self.BLOCK_SIZE, self.BLOCK_SIZE), 2)
+        return
+    
+    def drop_piece(self, y, x):
+        if self.selected_pos is not None:
+            prev_y, prev_x = self.selected_pos
+
+            (color, type) = self.board[prev_y][prev_x]
+            if type == 'pawn':
+                if abs(y - prev_y) <= 2 and abs(x - prev_x) == 0:
+                    self.board[y][x] = self.board[prev_y][prev_x]
+                    self.board[prev_y][prev_x] = None
+
+                
+
+            self.draw_board()
+            self.draw_pieces()
+
+    def move(self):
+            
+
+        return
+
+# ------- MANUAL Chess -------
+
+# only execute if game_env file was executed, not if the file is imported
+if __name__ == "__main__": 
+    env = Chess()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                x, y = pos[0] // env.BLOCK_SIZE, pos[1] // env.BLOCK_SIZE
+                env.select_piece(y, x)
+                
+                print(f"Clicked on block (y, x) format: ({y}, {x}) ")
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                x, y = pos[0] // env.BLOCK_SIZE, pos[1] // env.BLOCK_SIZE
+                env.drop_piece(y, x)
+            
+
+
+        pygame.display.flip()
+
+
+    pygame.quit()
+
+
