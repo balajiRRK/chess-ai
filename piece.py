80edb8eee633c6fa4578aea87ad8bad0e23bb793
balajiRRK <rbalajirrk@gmail.com>
2025-07-05 13:29:16 -0400

    fix rook movement - check for jumping over pieces - check for capturing own pieces

--- piece.py
+++ piece.py
@@ -19,7 +19,9 @@ class King(Piece):
         dy = abs(start[0] - end[0])
         dx = abs(start[1] - end[1])
 
-        return max(dx, dy) == 1
+        has_obstacle = board[end[0]][end[1]] is not None and board[end[0]][end[1]].color == self.color
+
+        return max(dx, dy) == 1 and not has_obstacle
     
 class Queen(Piece):
     def __init__(self, color):
@@ -33,67 +35,62 @@ class Rook(Piece):
         super().__init__(color, 'rook')
 
     def is_valid_move(self, board, start, end):
-        dy = abs(start[0] - end[0])
-        dx = abs(start[1] - end[1])
+        dy = end[0] - start[0]
+        dx = end[1] - start[1]
 
-        return True if (dx != 0 and dy == 0) or (dx == 0 and dy != 0) else False
+        if (dx == 0 and dy == 0) or (dx != 0 and dy != 0):
+            return False
+        
+        # sign checker
+        # 1 - 0 = 1 for positive step direction, 0 - 1 = -1 for negative
+        step_y = (dy > 0) - (dy < 0) 
+        step_x = (dx > 0) - (dx < 0)
+
+        y = start[0] + step_y
+        x = start[1] + step_x
+
+        # check for obstacles up until but not including ending position so rook doesn't jump over pieces
+        while (y, x) != end:
+            if board[y][x] is not None:
+                return False
+            y += step_y
+            x += step_x
+
+        # dont capture own piece
+        if board[end[0]][end[1]] is not None and board[end[0]][end[1]].color == self.color:
+            return False
+        
+        return True
 
 class Bishop(Piece):
     def __init__(self, color):
         super().__init__(color, 'bishop')
 
     def is_valid_move(self, board, start, end):
-        dy = start[0] - end[0]
-        dx = start[1] - end[1]
+        dy = end[0] - start[0]
+        dx = end[1] - start[1]
 
-        print(f"(dy, dx): ({dy}, {dx})")
-        print(dy > 0 and dx < 0)
-        if abs(dy) == abs(dx):
+        if abs(dy) != abs(dx):
+            return False
+        
+        step_y = 1 if dy > 0 else -1
+        step_x = 1 if dx > 0 else -1
 
-            has_obstacle = False
-            if dy > 0 and dx > 0:
-                for i in range(1, dy+1): # inclusive so it doesn't capture same-color pieces
-                    print(f"(dy, dx): ({dy}, {dx})")
-                    print(f"({start[0] - i}, {start[1] - i})")
-                    print(board[start[0] - i][start[1] - i])
-                    if board[start[0] - i][start[1] - i] is not None and board[start[0] - i][start[1] - i].color == self.color:
-                        has_obstacle = True
-                        print("has obstacle")
-                        print("")
+        y = start[0] + step_y
+        x = start[1] + step_x
 
-            elif dy > 0 and dx < 0:
-                print("right if branch ran")
-                for i in range(1, dy+1):
-                    print(f"(dy, dx): ({dy}, {dx})")
-                    print(f"({start[0] - i}, {start[1] + i})")
-                    print(board[start[0] - i][start[1] + i])
-                    if board[start[0] - i][start[1] + i] is not None and board[start[0] - i][start[1] + i].color == self.color:
-                        has_obstacle = True
-                        print("has obstacle")
-                        print("")
-
-            elif dy < 0 and dx < 0:
-                for i in range(1, abs(dy)+1):
-                    print(f"(dy, dx): ({dy}, {dx})")
-                    print(f"({start[0] - i}, {start[1] - i})")
-                    print(board[start[0] - i][start[1] - i])
-                    if board[start[0] + i][start[1] + i] is not None and board[start[0] + i][start[1] + i].color == self.color:
-                        has_obstacle = True
-                        print("has obstacle")
-                        print("")
+        # check for obstacles up until but not including ending position so bishop doesn't jump over pieces
+        while (y, x) != end: 
+            if board[y][x] is not None:
+                return False
+            y += step_y
+            x += step_x
             
-            elif dy < 0 and dx > 0:
-                for i in range(1, abs(dy)+1):
-                    print(f"(dy, dx): ({dy}, {dx})")
-                    print(f"({start[0] - i}, {start[1] - i})")
-                    print(board[start[0] - i][start[1] - i])
-                    if board[start[0] + i][start[1] - i] is not None and board[start[0] + i][start[1] - i].color == self.color:
-                        has_obstacle = True
-                        print("has obstacle")
-                        print("")
-
-
-        return True if abs(dy) == abs(dx) and not has_obstacle else False
+        # dont capture own piece
+        if board[end[0]][end[1]] is not None and board[end[0]][end[1]].color == self.color:
+            return False
+        
+        return True
 
 class Knight(Piece):
     def __init__(self, color):
